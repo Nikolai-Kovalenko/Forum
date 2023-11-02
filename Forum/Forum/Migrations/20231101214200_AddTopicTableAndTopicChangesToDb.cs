6f427276ea2d0b4e630cec839f94a7fc79e511c0
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-11-02 23:22:34 +0200

    topic IndexView

---
+++ Forum/Forum/Migrations/20231101214200_AddTopicTableAndTopicChangesToDb.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forum.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTopicTableAndTopicChangesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Topics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SectionId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastChangeTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeleteTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Topics_Sections_SectionId",
+                        column: x => x.SectionId,
+                        principalTable: "Sections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TopicsChanges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TopicId = table.Column<int>(type: "int", nullable: false),
+                    Field = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FromValue = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ToValue = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ChangeTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TopicsChanges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TopicsChanges_Topics_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Topics_SectionId",
+                table: "Topics",
+                column: "SectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TopicsChanges_TopicId",
+                table: "TopicsChanges",
+                column: "TopicId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TopicsChanges");
+
+            migrationBuilder.DropTable(
+                name: "Topics");
+        }
+    }
+}
